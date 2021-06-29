4fe9d94d6ed69fe76a02705908d5ee07585dc92d
bilalkirkgul <61188245+bilalkirkgul@users.noreply.github.com>
2021-06-29 20:18:32 +0300

    CustomRoute2

---
+++ 07-CustomRoute/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _07_CustomRoute.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("Anasayfa")]
+        public IActionResult HomePage()
+        {
+            return View();
+        }
+        [Route("BilalKirkgulBlog")]
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        [Route("Galeri")]
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+    }
+}
